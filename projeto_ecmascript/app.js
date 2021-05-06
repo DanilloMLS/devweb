dcfff0141b1ed5be5abb43833d1a1d8891a998c2
DanilloMLS <moraesdanillo10@gmail.com>
2021-05-05 21:15:11 -0300

    Uso de localStorage

--- projeto_ecmascript/app.js
+++ projeto_ecmascript/app.js
@@ -11,6 +11,32 @@ class Despesa {
 
 }
 
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    //define um novo id para uma nova despesa
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
+
+    //para usar localStorage, devemos passar o objeto como JSON
+    gravar(d) {
+        //localStorage.setItem('despesa', JSON.stringify(d))
+        let id = this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id',id)
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
     //é possível usar .value na variável (ano.value)
     let ano = document.getElementById('ano').value
@@ -24,10 +50,5 @@ function cadastrarDespesa() {
         ano,mes,dia,tipo,descricao,valor
     )
     
-    gravar(despesa)
-}
-
-//para usar localStorage, devemos passar o objeto como JSON
-function gravar(d) {
-    localStorage.setItem('despesa', JSON.stringify(d))
+    bd.gravar(despesa)
 }
