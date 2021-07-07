b7f22184d2aaed327cce10fc1a5945428206d06b
DanilloMLS <moraesdanillo10@gmail.com>
2021-07-07 20:47:29 -0300

    Criação do miniframework

---
+++ 14_php_com_mvc/miniframework/App/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App\Controllers;
+    
+    class IndexController {
+        public function index() {
+            echo 'Chegamos ao IndexController e disparamos a action index';
+        }
+
+        public function sobreNos() {
+            echo 'Chegamos ao IndexController e disparamos a action sobreNos';
+        }
+    }
+    
+
+?>
---
+++ 14_php_com_mvc/miniframework/public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Route;
+
+require_once "../vendor/autoload.php";
+
+    $route = new Route;
+    /* echo 'isso está funcionando';
+    
+    echo '<hr>';
+    
+    echo '<pre>';
+    print_r($route->getUrl());
+    echo '</pre>';
+
+    echo '<hr>';
+
+    echo '<pre>';
+    print_r($route->getRoutes());
+    echo '</pre>'; */
+?>
